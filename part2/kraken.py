b9c8c67b43bd89be6afa68c0f3c397c015f17231
Adrian Garza <adrianhgarza@gmail.com>
2017-01-11 23:37:03 -0800

    algorithm for part2

---
+++ part2/kraken.py
@@ -0,0 +1,10 @@
+def krakenCount(m, n):
+    #create a board, fill the first rows and columns with 0
+    board = [[0 for i in range(m)] for row in range(n)]
+    #now fill the first row and first column with 1
+
+    
+    #every square is the sum of its upper, left, and upper left neighbors
+
+
+    return board
---
+++ part2/tester.py
@@ -0,0 +1,8 @@
+import sys
+import os
+from kraken import krakenCount
+
+_m = int(raw_input())
+_n = int(raw_input())
+
+print krakenCount(_m, _n)
