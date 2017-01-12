32de33099c014b82ea4443e570a9de34439d4b62
Adrian Garza <adrianhgarza@gmail.com>
2017-01-12 09:23:59 -0800

    fill board with 1s

--- part2/kraken.py
+++ part2/kraken.py
@@ -2,7 +2,10 @@ def krakenCount(m, n):
     #create a board, fill the first rows and columns with 0
     board = [[0 for i in range(m)] for row in range(n)]
     #now fill the first row and first column with 1
-
+    for i in range(0, len(board[0])):
+        board[0][i] = 1
+    for j in range(0, len(board)):
+        board[j][0] = 1
     
     #every square is the sum of its upper, left, and upper left neighbors
 
--- part2/tester.py
+++ part2/tester.py
@@ -5,4 +5,5 @@ from kraken import krakenCount
 _m = int(raw_input())
 _n = int(raw_input())
 
-print krakenCount(_m, _n)
+for row in krakenCount(_m, _n):
+    print row
